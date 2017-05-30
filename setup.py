3983fb7ab5f89c72cd4bf4d3bedb042aed19c808
Chris Hudson <c.ahudson84@yahoo.com>
2017-05-30 08:24:15 -0700

    Update to readme and setup.

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-"""Setup for http server."""
+"""Setup for http concurrency server."""
 from setuptools import setup
 
 dependencies = []
@@ -8,12 +8,12 @@ extra_packages = {
 
 setup(
     name='http-server',
-    description='Client sends message and server sends OK back.',
+    description='Client sends message and server sends content back.',
     version='0.1',
     author='Carlos Cadena, Chris Hudson',
     author_email='cs.cadena@gmail.com, c.ahudson84@yahoo.com',
     license='MIT',
-    py_modules='client, server',
+    py_modules='server_client, server, concurrent_client, concurrent',
     package_dir={'': 'src'},
     install_requires=dependencies,
     extras_require=extra_packages,
