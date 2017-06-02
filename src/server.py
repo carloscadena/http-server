fe5959b9e3adb5831e1269a06a98720ae4483008
Christopher Hudson <c.ahudson84@yahoo.com>
2017-06-01 21:11:23 -0700

    Update server.py

--- src/server.py
+++ src/server.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-"""Server for http-server echo assignment."""
+"""Server for http-server step2 assignment."""
 from __future__ import unicode_literals
 import socket  # pragma: no cover
 import sys  # pragma: no cover
